8b3928a18998de347d2b7f648bab81bdb9387364
aleksandr.z <gently.whitesnow@outlook.com>
2026-08-02 13:44:29 +0300

    fix(contracts): MAIN-357 упростить Int64String
    Решения:
    - OpenAPI проверяет компактную лексическую форму и длину 19 символов, а точный диапазон остаётся на WireInt64.
    - Специальный Int64-гейт и добавленная ради него прямая зависимость удалены; воспроизводимость держат штатные contract-гейты.
    
    Отброшено:
    - Точное кодирование long.MaxValue в regex: раздувает каждый generated DTO и дублирует runtime-границу.
    
    Ограничения:
    - Публичный строковый wire-формат и внутренние long не менялись.

--- backend/Bugget.Contracts/Generated/AnalyticsContracts.g.cs
+++ backend/Bugget.Contracts/Generated/AnalyticsContracts.g.cs
@@ -137,7 +137,8 @@ namespace Bugget.Contracts.Analytics.Generated
 
         [System.Text.Json.Serialization.JsonPropertyName("report_id")]
         [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
-        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]{0,17}|[1-8][0-9]{18}|9[01][0-9]{17}|92[01][0-9]{16}|922[0-2][0-9]{15}|9223[0-2][0-9]{14}|92233[0-6][0-9]{13}|922337[01][0-9]{12}|92233720[0-2][0-9]{10}|922337203[0-5][0-9]{9}|9223372036[0-7][0-9]{8}|92233720368[0-4][0-9]{7}|922337203685[0-3][0-9]{6}|9223372036854[0-6][0-9]{5}|92233720368547[0-6][0-9]{4}|922337203685477[0-4][0-9]{3}|9223372036854775[0-7][0-9]{2}|922337203685477580[0-6]|9223372036854775807)$")]
+        [System.ComponentModel.DataAnnotations.StringLength(19)]
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]*)$")]
         public string Report_id { get; set; }
 
         /// <summary>
@@ -293,7 +294,8 @@ namespace Bugget.Contracts.Analytics.Generated
 
         [System.Text.Json.Serialization.JsonPropertyName("report_id")]
         [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
-        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]{0,17}|[1-8][0-9]{18}|9[01][0-9]{17}|92[01][0-9]{16}|922[0-2][0-9]{15}|9223[0-2][0-9]{14}|92233[0-6][0-9]{13}|922337[01][0-9]{12}|92233720[0-2][0-9]{10}|922337203[0-5][0-9]{9}|9223372036[0-7][0-9]{8}|92233720368[0-4][0-9]{7}|922337203685[0-3][0-9]{6}|9223372036854[0-6][0-9]{5}|92233720368547[0-6][0-9]{4}|922337203685477[0-4][0-9]{3}|9223372036854775[0-7][0-9]{2}|922337203685477580[0-6]|9223372036854775807)$")]
+        [System.ComponentModel.DataAnnotations.StringLength(19)]
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]*)$")]
         public string Report_id { get; set; }
 
         [System.Text.Json.Serialization.JsonPropertyName("title")]
@@ -326,7 +328,8 @@ namespace Bugget.Contracts.Analytics.Generated
 
         [System.Text.Json.Serialization.JsonPropertyName("report_id")]
         [System.ComponentModel.DataAnnotations.Required(AllowEmptyStrings = true)]
-        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]{0,17}|[1-8][0-9]{18}|9[01][0-9]{17}|92[01][0-9]{16}|922[0-2][0-9]{15}|9223[0-2][0-9]{14}|92233[0-6][0-9]{13}|922337[01][0-9]{12}|92233720[0-2][0-9]{10}|922337203[0-5][0-9]{9}|9223372036[0-7][0-9]{8}|92233720368[0-4][0-9]{7}|922337203685[0-3][0-9]{6}|9223372036854[0-6][0-9]{5}|92233720368547[0-6][0-9]{4}|922337203685477[0-4][0-9]{3}|9223372036854775[0-7][0-9]{2}|922337203685477580[0-6]|9223372036854775807)$")]
+        [System.ComponentModel.DataAnnotations.StringLength(19)]
+        [System.ComponentModel.DataAnnotations.RegularExpression(@"^(0|[1-9][0-9]*)$")]
         public string Report_id { get; set; }
 
         [System.Text.Json.Serialization.JsonPropertyName("title")]
